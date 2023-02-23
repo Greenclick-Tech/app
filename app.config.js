7426e8a792b46e9aba065ece5df499824195fd14
sahilsian <sahilsian101@gmail.com>
2023-02-22 20:18:52 -0400

    Version Update

--- app.config.js
+++ app.config.js
@@ -23,7 +23,7 @@ export default () => ({
     ios: {
       supportsTablet: false,
       bundleIdentifier: "org.name.greenclick",
-      buildNumber: "24",
+      buildNumber: "25",
       infoPlist: {
         NSLocationWhenInUseUsageDescription: "This app uses your location to retrieve nearby hotels & unlock the greenclick lock box when you are in clock proximity.",
         NSLocationAlwaysUsageDescription: "This app requires your location to verify if you are in close proximity of the greenclick lock box.",
