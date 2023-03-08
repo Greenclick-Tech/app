f4369fd58957d38204b090ff231e6f459fcf66a5
sahilsian <sahilsian101@gmail.com>
2023-03-07 22:16:42 -0400

    App 1.0.6 Fixed/Redone Help/Account Pages Routing (Maybe) Fixed Search Bar not loading in properly Fixed text cutoff on newer devices for NoLocationPermissions on Map Page

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default () => ({
   expo: {
     name: "Greenclick",
     slug: "greenclick",
-    version: "1.0.6",
+    version: "1.0.7",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
