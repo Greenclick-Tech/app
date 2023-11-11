46057237622ef949a76a53868c05e8bb549573bf
sahilsian <sahilsian101@gmail.com>
2023-11-11 12:50:58 -0400

    Fixing Notification Bug

--- src/pages/screens/appScreens/home-page/index.js
+++ src/pages/screens/appScreens/home-page/index.js
@@ -249,20 +249,8 @@ const HomePage = ({ navigation, route }) => {
   async function registerForPushNotificationsAsync() {
     let token;
     if (Device.isDevice) {
-      const { status: existingStatus } = await Notifications.getPermissionsAsync();
-      let finalStatus = existingStatus;
-      if (existingStatus !== 'granted') {
-        const { status } = await Notifications.requestPermissionsAsync();
-        finalStatus = status;
-      }
-      if (finalStatus !== 'granted') {
-        alert('Failed to get push token for push notification!');
-        return;
-      }
       token = (await Notifications.getExpoPushTokenAsync()).data;
       savePushToken(token)
-    } else {
-      alert('Must use physical device for Push Notifications');
     }
 
     if (Platform.OS === 'android') {
